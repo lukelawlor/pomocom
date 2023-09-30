735e7d4ebc79c59ce8b1858c3598583255d37e71
Luke Lawlor <lklawlor1@gmail.com>
2023-09-30 17:15:46 -0400

    fixed position calculation for dzen2 msg script

--- config/msg_dzen.sh
+++ config/msg_dzen.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 # This script will output text from stdin with dzen2
 
-# screen dimensions
+# your screen dimensions
 screen_width=1280
 screen_height=1024
 
 # message styling
-x=$[screen_width / 2 - width / 2]
-y=$[screen_height / 2 - height / 2]
 bg=blue
 fg=white
-#bg=black
-#fg=#aba7ff
 seconds=60
 height=30
 width=300
+x=$[screen_width / 2 - width / 2]
+y=$[screen_height / 2 - height / 2]
 
 # output the message
 killall dzen2
